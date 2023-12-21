4706bd483d8bdf9af66fe15531c03f4e9b467c55
Nebiyu <nebiyuhiyabu@gmail.com>
2023-12-21 10:48:54 +0300

    Optimizing the routing protocol

--- requests_aes.py
+++ requests_aes.py
@@ -1,4 +1,10 @@
 #!/bin/bash/python3
+"""
+This module is responsible for the configuration of the routes to
+encrypt and decrypt the data of the user.
+
+Below are some libraries that will be used in this module
+"""
 from flask import Flask, request, make_response, render_template, send_file
 import os
 import zipfile
@@ -9,10 +15,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    Defining a function to render the landing page
+    """
     return render_template('index2.html')
 
 @app.route('/process', methods=['POST'])
 def process():
+    """
+    Route to test the flask app
+    """
     if request.method == 'POST':
         user_name = request.form['fname']
         return render_template('results.html', user_name=user_name)
@@ -22,6 +34,10 @@ def process():
 
 @app.route('/upload', methods=['POST'])
 def encrypt():
+    """
+    This function will handle the route '/upload' and
+    later encrypt the file uploaded by the user
+    """
     if request.method == 'POST':
         if 'file' not in request.files:
             return "No files"
@@ -31,108 +47,70 @@ def encrypt():
         if file.filename == '':
             return "No file selected"
         
-        file.save(file.filename)  # Save the uploaded file
+        file_path = file.filename
+        file.save(file_path)  # Save the uploaded file
 
         password = request.form['password']
-        # subprocess.run(
-        #     [
-        #         "openssl", "genpkey", "-algorithm", "RSA", "-out", "private.pem",
-        #         "-pass", f"pass:{password}"
-        #     ]
-        # )
-        # subprocess.run(
-        #     [
-        #         "openssl", "rsa", "-in", "./private.pem", "-pubout", "-out", "public.pem",
-        #         "-passin", f"pass:{password}"
-        #     ]
-        # )
+        
+        encrypt_cmd = [
+                "openssl", "enc", "-aes-256-cbc", "-salt",
+                "-in", file_path, "-pass", f"pass:{password}"
+                ]
 
-        with open(file.filename, 'rb') as f:
-            file_content = f.read()
+        try:
+            encrypted = subprocess.check_output(encrypt_cmd)
+            #encrypted_text = encrypted.decode('utf-8')
 
-        with open('encrypted_file.enc', 'wb') as encrypted_file:
-            subprocess.run(
-                [
-                    "openssl", "enc", "-aes-256-cbc", "-salt","-in", "-", "-out", "-",
-                    "-pass", f"pass: {password}"
-                ],
-                input=file_content,
-                stdout=encrypted_file
-            )
-        os.remove(f'{file.filename}')
+            encrypted_file_path = "encrypted_file.enc"
+            with open(encrypted_file_path, 'wb') as encrypted_file:
+                encrypted_file.write(encrypted)
 
-        return render_template('index2.html')
-    
+            return send_file(encrypted_file_path, as_attachment=True)
+
+        except subprocess.CalledProcessError as e:
+            return f"Error: {e}"
+       
     return render_template('error.html')
 
-@app.route('/download')
-def encrypted_download():
-    # files = ['./private.pem', './public.pem', './encrypted_file.enc']
-    # zip_file = 'encrypted_data.zip'
-    file = "./encrypted_file.enc"
-
-    # with zipfile.ZipFile(zip_file, 'w') as zipf:
-    #     for file_path in files:
-    #         zipf.write(file_path)
-
-    # os.remove('private.pem')
-    # os.remove('public.pem')
-    # os.remove('encrypted_file.enc')
-    
-
-    return send_file(file, as_attachment=True, download_name='encrypted_file.enc')
 
 @app.route('/decrypt', methods=['POST'])
 def decrypt_file():
-    if request.method == 'POST':
+    """
+    This route decrypts the encrypted file uploaded by the user
+    """
+    if request.method=='POST':
+        
         #if 'file' not in request.files['encrypted']:
          #   return "No files"
-        
+
         encrypted_file = request.files['encrypted']
-        # private_key = request.files['private']
         password = request.form['password']
 
-        if encrypted_file.filename == '':
-            return "No file selected"
-        # if private_key.filename == '':
-        #     return "No file selected"
+        encrypted_file_path = encrypted_file.filename
+        encrypted_file.save(encrypted_file_path)
+
+        decrypt = [
+                "openssl", "enc", "-d", "-aes-256-cbc",
+                "-in", encrypted_file_path, "-pass", f"pass:{password}"
+                ]
+        try:
+            decrypted_output = subprocess.check_output(decrypt)
+
+            # Encode the decrypted output as UTF-8
+            decrypted_text = decrypted_output.decode('utf-8')
+
+            # Write the UTF-8 encoded content to a new file
+            decrypted_file_path = "decrypted_file.txt"
+            with open(decrypted_file_path, 'w', encoding='utf-8') as decrypted_file:
+                decrypted_file.write(decrypted_text)
+
+            return send_file(decrypted_file_path, as_attachment=True)
         
-        encrypted_file.save(encrypted_file.filename)
-        # private_key.save(private_key.filename)
+        except subprocess.CalledProcessError as e:
+            return f"Error: {e}"
 
-        with open(encrypted_file.filename, 'rb') as f:
-          file_content = f.read()
 
-        # with open(private_key.filename, 'rb') as f:
-        #   key_content = f.read()
-
-        
-        with open('decrypted_file', 'w') as d:
-
-            subprocess.run(
-                ["openssl", "enc", "-d", "-aes-256-cbc","-in", "-", "-out", "-",
-                 "-pass", f"pass:{password}"],
-                input=file_content,
-                stdout=d
-            )
-        
-        return render_template('index2.html')
-    
     return render_template("error.html")
-
-@app.route('/decrypt/download')
-def d_download():
-    file = './decrypted_file'
-    
-    with codecs.open(file, 'r', encoding='utf-8') as file:
-        file_content = file.read()
-
-    # Create a response with modified content and specific encoding
-    response = make_response(file_content)
-    response.headers['Content-Disposition'] = 'attachment; filename=example.txt'
-    response.headers['Content-Type'] = 'text/plain; charset=utf-8'
-
-    return send_file(response, as_attachment=True, download_name="Decrypted_file")
 
 
 if __name__ == '__main__':
